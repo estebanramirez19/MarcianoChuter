86add539b59f7858367c6c2da988639569dcb15a
alumno/esteban_ramirez <esteban.ramirez19@inacapmail.cl>
2025-08-14 23:32:51 -0400

    agregando movimiento en la imagen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,34 +1,51 @@
 import sys
 import pygame
-from MarcianoChuter.settings import Settings
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.clock = pygame.time.Clock().
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
+
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width , self.settings.screen_heigth)
         )
         pygame.display.set_caption("Invasion de Marcianekes")
+
+        self.ship = Ship(self)
         
-        
+    ## creamos dos funciones _check_events y _update_screen para que run game no este tan poblado de codigo
+
+    def _check_events(self):
+        # buscamos eventos de teclado y raton para salir del juego dentro de pygame
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                
+
+
+    def _update_screen(self): ## encargado de la configuracion de la pantalla
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            ## hacemos visible la ultima pantalla dibujada
+            pygame.display.flip()
 
     def run_game(self):
         #iniciamos el bucle del juego
         while True:
-            #buscamos eventos de teclado y raton para salir del juego dentro de pygame
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-
-            self.screen.fill(self.settings.bg_color)
-
-            ##hacemos visible la ultima pantalla dibujada
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -8,11 +8,18 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #cargar la imagen de la nave y obtener su rect
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images\ship.bmp')
         self.rect = self.image.get_rect()
 
         #esto coloca la imagen del la nueva nave inicialmente en el centro
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #bandera de movimiento
+        self.moving_right = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         #dibuja la nave en su ubicacion actual
